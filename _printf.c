5ac073664525f96ca93c4f0048756cda3129fcbf
paul-mulatya <mulatyapaul44@gmail.com>
2023-04-26 16:42:52 +0000

    betty to 80

--- _printf.c
+++ _printf.c
@@ -16,30 +16,31 @@ int _printf(const char *format, ...)
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 	return (-1);
 	while (format[j])
-	{	l = 0;
-		if (format[j] == '%')
+	{
+	l = 0;
+	if (format[j] == '%')
+	{
+		if (!format[j + 1] || (format[j + 1] == ' ' && !format[j + 2]))
 		{
-			if (!format[j + 1] || (format[j + 1] == ' ' && !format[j + 2]))
-			{
-				k = -1;
-				break;
-			}
-			l += getprintfunc(format[j + 1], val);
-			if (l == 0)
-			k += _putchar(format[j + 1]);
-			if (l == -1)
-			{
-				k = -1;
-			}
-			j++;
+			k = -1;
+			break;
 		}
-		else
+		l += getprintfunc(format[j + 1], val);
+		if (l == 0)
+		k += _putchar(format[j + 1]);
+		if (l == -1)
 		{
-			(k == -1) ? (_putchar(format[j])) : (k += _putchar(format[j]));
+			k = -1;
 		}
 		j++;
-		if (k != -1)
-		k += l;
+	}
+	else
+	{
+		(k == -1) ? (_putchar(format[j])) : (k += _putchar(format[j]));
+	}
+	j++;
+	if (k != -1)
+	k += l;
 	}
 	va_end(val);
 	return (k);
