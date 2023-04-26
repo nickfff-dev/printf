9f23e80ab50844c9a1c7e3533a5cedd200aeed9d
nickfff-dev <hunchographics@gmail.com>
2023-04-26 17:11:22 +0000

    undid change from cli

--- _printf.c
+++ _printf.c
@@ -16,42 +16,30 @@ int _printf(const char *format, ...)
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 	return (-1);
 	while (format[j])
-	{
-	l = 0;
-	if (format[j] == '%')
-	{
-		if (!format[j + 1] || (format[j + 1] == ' ' && !format[j + 2]))
-<<<<<<< HEAD
+	{	l = 0;
+		if (format[j] == '%')
 		{
-			k = -1;
-			break;
-		}
-		l += getprintfunc(format[j + 1], val);
-		if (l == 0)
-		k += _putchar(format[j + 1]);
-		if (l == -1)
-		{
-			k = -1;
-=======
-		{
-			k = -1;
-			break;
->>>>>>> bc596384acf72a71e8611f3fbbe38daf242a78c9
-		}
-		l += getprintfunc(format[j + 1], val);
-		if (l == 0)
+			if (!format[j + 1] || (format[j + 1] == ' ' && !format[j + 2]))
+			{
+				k = -1;
+				break;
+			}
+			l += getprintfunc(format[j + 1], val);
+			if (l == 0)
 			k += _putchar(format[j + 1]);
-		if (l == -1)
-			k = -1;
+			if (l == -1)
+			{
+				k = -1;
+			}
+			j++;
+		}
+		else
+		{
+			(k == -1) ? (_putchar(format[j])) : (k += _putchar(format[j]));
+		}
 		j++;
-	}
-	else
-	{
-		(k == -1) ? (_putchar(format[j])) : (k += _putchar(format[j]));
-	}
-	j++;
-	if (k != -1)
-	k += l;
+		if (k != -1)
+		k += l;
 	}
 	va_end(val);
 	return (k);
