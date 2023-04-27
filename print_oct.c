91f08173450dd28dee8935b1e09575796a2d54b9
nickfff-dev <hunchographics@gmail.com>
2023-04-27 19:45:28 +0000

    error

--- print_oct.c
+++ print_oct.c
@@ -1,38 +1,28 @@
 #include "main.h"
 
 /**
-* print_oct - the function prints an oct
-* @args: parameter of type va_list
-* Return: int
-*/
+  * print_oct - the function prints an oct
+  * @args: parameter of type va_list
+  * Return: int
+ */
 int print_oct(va_list args)
 {
 	unsigned int num = va_arg(args, unsigned int);
-	int x;
-	int *list;
-	unsigned int div = 0;
+	int x = 0;
+	unsigned int div = 1;
 	unsigned int num_aux = num;
 
-	while (num / 8 != 0)
+	while (num_aux > 7)
 	{
-		num /= 8;
-		div++;
-	}
-	div++;
-	list = malloc(div * sizeof(int));
-	x = 0;
-	while (x < div)
-	{
-		list[x] = num_aux % 8;
+		div *= 8;
 		num_aux /= 8;
-		x++;
 	}
-	x = div - 1;
-	while (x >= 0)
+	while (div != 0)
 	{
-		_putchar(list[x] + '0');
-		x--;
+		_putchar((num / div) + '0');
+		x++;
+		num %= div;
+		div /= 8;
 	}
-	free(list);
-	return (div);
+	return (x);
 }
